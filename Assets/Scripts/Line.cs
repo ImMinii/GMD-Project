12efe56fe3fa325c8d630194cc76b3ef59a3ac7f
ImMinii <simonbanh@hotmail.com>
2025-05-10 14:30:56 +0200

    Et par settings på tweaks og starten på en linje der forbinder
    Rope physics er noget jeg ikke magter

---
+++ Assets/Scripts/Line.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    public Transform objectA; // Assign Player 1
+    public Transform objectB; // Assign Player 2
+    private LineRenderer lineRenderer;
+
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 2; // Line needs two points
+    }
+
+    void Update()
+    {
+        if (objectA != null && objectB != null)
+        {
+            lineRenderer.SetPosition(0, objectA.position);
+            lineRenderer.SetPosition(1, objectB.position);
+        }
+    }
+}
